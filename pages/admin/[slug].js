63fc43339c4d4db80a70d512a4cb655f4616e976
Phone Zaw <phonezawuit@gmail.com>
2022-02-21 12:59:43 +0630

    add forms

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -6,9 +6,15 @@ import restaurants from "../../utils/data";
 function Restaurant({ restaurant }) {
   const router = useRouter();
   const [slug, setSlug] = useState(router.query.slug);
+  const [isOpened, setIsOpened] = useState(false);
 
   const handleClick = () => {
     // add Item
+    setIsOpened(true);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
   };
 
   return (
@@ -43,6 +49,18 @@ function Restaurant({ restaurant }) {
             <p className="item-price">{item.price || "MMK --"}</p>
           </div>
         ))}
+      </div>
+      <div className="add-form add-item-form">
+        <h1>Add Item Form</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="item-name">Item Name</label>
+          <input id="item-name" name="item-name" type="text" required />
+          <label htmlFor="item-price">Item Price</label>
+          <input id="item-price" name="item-price" type="text" required />
+          <label htmlFor="item-image">Item Image</label>
+          <input id="item-image" name="item-image" type="file" disabled />
+          <button type="submit">Add Item</button>
+        </form>
       </div>
       <FloatingBtn onClick={handleClick} />
     </>
--- pages/admin/home.js
+++ pages/admin/home.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Link from "next/link";
 import FloatingBtn from "../../components/admin/FloatingBtn";
 import Nav from "../../components/Nav";
@@ -5,8 +6,13 @@ import Restaurant from "../../components/Restaurant";
 import restaurants from "../../utils/data.json";
 
 function home() {
+  const [isOpened, setIsOpened] = useState(false);
   const handleClick = () => {
-    // add Restaurant
+    setIsOpened(true);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target.name.value);
   };
   return (
     <div className="admin-page">
@@ -29,6 +35,35 @@ function home() {
         ))
       ) : (
         <h2>No Restaurants found</h2>
+      )}
+      {/* Modal form for add restaurant */}
+      {isOpened && (
+        <div className="add-form">
+          <h1>Add Restaurant Form</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name</label>
+            <input id="name" name="name" type="text" />
+            <label htmlFor="image">Image</label>
+            <input id="image" name="image" type="file" disabled />
+            <label htmlFor="cuisine">Cuisine</label>
+            <input id="cuisine" name="cuisine" type="text" />
+            <label htmlFor="avgPrice">Average Price</label>
+            <input id="avgPrice" name="avgPrice" type="text" />
+            <label htmlFor="address">Address</label>
+            <input id="address" name="address" type="text" />
+            <label htmlFor="phone">Phone No.</label>
+            <input id="phone" name="phone" type="text" />
+            {/* <div className="add-item">
+            <label htmlFor="item-name">Item Name</label>
+            <input id="item-name" name="item-name" type="text" />
+            <label htmlFor="item-price">Item Price</label>
+            <input id="item-price" name="item-price" type="text" />
+            <label htmlFor="item-image">Item Image</label>
+            <input id="item-image" name="item-image" type="file" disabled />
+          </div> */}
+            <button type="submit">Add Restaurant</button>
+          </form>
+        </div>
       )}
       <FloatingBtn onClick={handleClick} />
     </div>
